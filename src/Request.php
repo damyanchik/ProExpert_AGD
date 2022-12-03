805238cd020e101cf8e097c000b4b1cf03a56d2c
damyanchik <d4mian.ndz@gmail.com>
2022-12-03 19:58:27 +0100

    Request refactor

--- src/Request.php
+++ src/Request.php
@@ -8,49 +8,49 @@ class Request
 {
     public static function post($key, $trim = false): mixed
     {
-        if (isset($_POST[$key]))
-            return ($trim) ? trim(strip_tags($_POST[$key])) : $_POST[$key];
+        if (!isset($_POST[$key]))
+            return NULL;
 
-        return NULL;
+        return ($trim) ? trim(strip_tags($_GET[$key])) : $_GET[$key];
     }
 
     public static function isPost($key): bool
     {
-        if (isset($_POST[$key]))
-            return true;
-        else
+        if (!isset($_POST[$key]))
             return false;
+        else
+            return true;
     }
 
     public static function emptyPost($key): bool
     {
-        if (empty($_POST[$key]))
-            return true;
-        else
+        if (!empty($_POST[$key]))
             return false;
+        else
+            return true;
     }
 
     public static function get($key, $trim = false): mixed
     {
-        if (isset($_GET[$key]))
-            return ($trim) ? trim(strip_tags($_GET[$key])) : $_GET[$key];
-        else
+        if (!isset($_GET[$key]))
             return NULL;
+
+        return ($trim) ? trim(strip_tags($_GET[$key])) : $_GET[$key];
     }
 
     public static function isGet($key): bool
     {
-        if (isset($_GET[$key]))
-            return true;
-        else
+        if (!isset($_GET[$key]))
             return false;
+        else
+            return true;
     }
 
     public static function emptyGet($key): bool
     {
-        if (isset($_GET[$key]) && empty($_GET[$key]))
-            return true;
-        else
+        if (!empty($_GET[$key]))
             return false;
+        else
+            return true;
     }
 }
